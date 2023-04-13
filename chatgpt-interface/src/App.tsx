195b83b279e81e0d9de9c49f60a564120ba91da3
makiroggers <makiroggers@gmail.com>
2023-04-12 19:32:48 -0500

    Add files

--- chatgpt-interface/src/App.tsx
+++ chatgpt-interface/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [responseText, setResponseText] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const response = await fetch('/api/chat', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: inputText }),
+    });
+    const data = await response.json();
+    setResponseText(data.reply);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGPT Interface</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+        placeholder="Type your message..."
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <p>{responseText}</p>
     </div>
   );
 }
